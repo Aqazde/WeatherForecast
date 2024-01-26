6395757c1a11a4284f3523a1882ffde89d81b50e
Aqazde <adil64489@gmail.com>
2024-01-26 15:23:56 +0600

    changed

--- js/openWeather.js
+++ js/openWeather.js
@@ -3,4 +3,4 @@
 // This file can be used specifically for handling OpenWeather API calls and any related functions
 // For now, it contains only the API key, but you can expand it with additional functionality in the future
 
-export const openWeatherApiKey = 'API Key';
+export const openWeatherApiKey = 'API Key ';
